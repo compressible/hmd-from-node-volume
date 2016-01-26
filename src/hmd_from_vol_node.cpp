9ba616edb2ada74245bd4b5da05baa3547d63d63
xd <xd>
2016-01-26 22:21:46 +0800

    Beginning to track hmd_from_vol_node.cpp.

---
+++ src/hmd_from_vol_node.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+  AUTHOR & COPYRIGHT
+    Copyright (c) 2008-2024 by XIE-Yuanfeng.
+    E-Mail: 
+
+  LICENSE
+    This code is for personal use only. 
+
+  DESCRIPTION
+    Export all the data in a case to hmd file.
+
+  CHANGES
+    Put notes on the major changes during your development.
+
+  FILE-ENCODING
+    UTF-8 without signature.
+ *******************************************************************************/
+
+//------------------------------------------------------------------------------------------
+#include <hybase.hpp>
+#include <vector>
+#include <string>
+
+//#include <hmdfs.h>
+using namespace std;
+
+//------------------------------------------------------------------------------------------
+int main()
+{
+    hy_msg("Convert Volume-Node data to HMD format");
+    return 0;
+}
+
